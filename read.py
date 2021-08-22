6ec9c1d5133c8bf3bb384fadc4d2519761b5af6c
Remi Chen <yipin987@gmail.com>
2021-08-22 17:11:49 +0800

    add fileter reviews

--- read.py
+++ read.py
@@ -11,5 +11,12 @@ with open('reviews.txt', 'r') as f:
 sum = 0
 for i in range(len(data)):
 	sum = sum + len(data[i])
-
 print('每筆平均留言長度:', sum / len(data))
+
+new = []
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+print('一共有', len(new), '筆留言小於100')
+
+print(new[0])
