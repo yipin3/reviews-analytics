04b05465d2c05eccbc1645663c3c789a3c3214be
Remi Chen <yipin987@gmail.com>
2021-08-22 15:53:13 +0800

    calc avg reviews

--- read.py
+++ read.py
@@ -6,6 +6,10 @@ with open('reviews.txt', 'r') as f:
 		count += 1
 		if count % 10000 == 0:
 			print(len(data))
+	print('讀完了，總共有', len(data), '筆資料')
 
+sum = 0
+for i in range(len(data)):
+	sum = sum + len(data[i])
 
-print(data[0])
+print('每筆平均留言長度:', sum / len(data))
